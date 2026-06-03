729b9c5298802f8a2140758feba5e59663fb4335
Keven Fernandes <kevenpyw@gmail.com>
2026-06-03 00:57:31 -0300

    feat: create mock data for Instagram posts

---
+++ src/mocks/postsInstagram.ts
@@ -0,0 +1,32 @@
+export const MOCK_POST_INSTAGRAM = [
+  {
+    id: "1",
+    address: "#",
+    alt: "post do instagram",
+    urlImage: "/plantas/planta3-instagram.jpg",
+  },
+  {
+    id: "2",
+    address: "#",
+    alt: "post do instagram",
+    urlImage: "/plantas/planta2-instagram.jpg",
+  },
+  {
+    id: "3",
+    address: "#",
+    alt: "post do instagram",
+    urlImage: "/plantas/planta4-instagram.jpg",
+  },
+  {
+    id: "4",
+    address: "#",
+    alt: "post do instagram",
+    urlImage: "/plantas/planta5-instagram.jpg",
+  },
+  {
+    id: "5",
+    address: "#",
+    alt: "post do instagram",
+    urlImage: "/plantas/planta1-instagram.jpg",
+  },
+];
